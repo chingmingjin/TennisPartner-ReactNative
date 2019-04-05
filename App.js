100842971ec17d5d30776fb190e38a2ac4094d38
Ivan <grubi.ivan@gmail.com>
2019-04-05 13:36:36 +0200

    TennisPartner written in ReactNative

---
+++ App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text } from 'native-base';
+export default class AnatomyExample extends Component {
+  render() {
+    return (
+      <Container>
+        <Header>
+          <Left>
+            <Button transparent>
+              <Icon name='menu' />
+            </Button>
+          </Left>
+          <Body>
+            <Title>Header</Title>
+          </Body>
+          <Right>
+          <Button transparent>
+              <Icon name='search' />
+            </Button>
+          </Right>
+        </Header>
+        <Content>
+          <Text>
+            This is Content Section
+          </Text>
+        </Content>
+        <Footer>
+          <FooterTab>
+            <Button full>
+              <Text>Footer</Text>
+            </Button>
+          </FooterTab>
+        </Footer>
+      </Container>
+    );
+  }
+}
