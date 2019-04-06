4f8e96fd4a31a413dbbcdf04d257593efbf24bd8
Ivan <grubi.ivan@gmail.com>
2019-04-06 20:26:43 +0200

    UI changes

---
+++ screens/home.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import { Alert } from 'react-native';
+import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text, StyleProvider } from 'native-base';
+
+import getTheme from '../native-base-theme/components';
+import commonColor from '../native-base-theme/variables/commonColor';
+
+
+export default class Home extends Component {
+    render() {
+      return (
+        <StyleProvider style={getTheme(commonColor)}>
+        <Container>
+          <Header>
+            <Left>
+              <Button transparent
+              onPress={() => this.props.navigation.openDrawer()}>
+                <Icon name='menu' />
+              </Button>
+            </Left>
+            <Body>
+              <Title style={{ color: "white" }}>Tennis Partner</Title>
+            </Body>
+            <Right>
+            <Button transparent 
+            onPress={() => {
+               Alert.alert('You tapped the button!');
+            }}>
+                <Icon name='search' />
+              </Button>
+            </Right>
+          </Header>
+          <Content>
+            <Text>
+              This is Content Section
+            </Text>
+          </Content>
+          <Footer>
+            <FooterTab>
+              <Button vertical active>
+              <Icon type="FontAwesome" name="users" />
+                <Text>Igrači</Text>
+              </Button>
+              <Button vertical>
+              <Icon type="MaterialCommunityIcons" name="tennis" />
+                <Text>Tereni</Text>
+              </Button>
+            </FooterTab>
+          </Footer>
+        </Container>
+        </StyleProvider>
+      );
+    }
+  }
