8f607a46a359136de87be8ecc5055730ade282b5
Ivan <grubi.ivan@gmail.com>
2019-07-25 15:54:47 +0200

    UI fixes

--- src/screens/chat.js
+++ src/screens/chat.js
@@ -246,6 +246,7 @@ class Chat extends Component {
                 backgroundColor: color("#1976d2").darken(0.2).hex()
               }}
               containerStyle={{
+                paddingTop: 0,
                 paddingLeft: 0,
                 backgroundColor: "#1976d2",
                 ...Platform.select({
